beb06749ea4c508cd528d416072c7f7999f2a87d
vansammi05 <134444161+vansammi05@users.noreply.github.com>
2023-05-25 16:48:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+document.getElementById('btn3').addEventListener('click',function(){
+    location.href = 'file:///C:/CLOCK%20USING%20HTML%20CSS%20AND%20JS/index.html'
+})
